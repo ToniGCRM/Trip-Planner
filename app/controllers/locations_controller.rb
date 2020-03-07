5f7a684a403ca2030b866e15a8d73c59b8272f68
Toni Galata <batscott@gmail.com>
2020-03-07 13:18:22 -0700

    merge pull

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,13 +1,43 @@
 class LocationsController < ApplicationController
-  def index
+    before_action :set_location, only: [:show, :edit, :update, :destroy]
+    before_action :set_trip
+    def index
+      @location = @trip.locations.all
+    end
+    def show
+    end
+    def new
+      @location = @trip.locations.new
+    end
+    def create
+      @location = @trip.locations.new(location_params)
+      if @location.save
+        redirect_to location_path
+      else
+        render :new
+      end
+    end
+    def edit
+    end
+    def update
+      if @location.update(location_params)
+        redirect_to location_path
+      else
+        render :edit
+      end
+    end
+    def destroy
+      @location.destroy
+      redirect_to location_path
+    end
+    private
+    def set_trip
+      @trip = trip.find(params[:parent_id])
+    end
+    def set_location
+      @location = @trip.locations.find(params[:id])
+    end
+    def location_params
+      params.require(:location).permit(:name, :description)
+    end
   end
-
-  def show
-  end
-
-  def new
-  end
-
-  def edit
-  end
-end
